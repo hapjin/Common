8d5a52f48c45c1b5f9cf960df0aae582ab963b62
langzhaozhi <langzhaozhi@sina.cn>
2015-02-03 11:09:19 +0800

    测试打印调整下

--- src/test/java/org/langzhaozhi/util/TestIntHash.java
+++ src/test/java/org/langzhaozhi/util/TestIntHash.java
@@ -6,6 +6,7 @@ public class TestIntHash {
     public static void main(String [] args) {
         int initialCapacity = 2000 * 10000;
 
+        //对比IntHash与HashMap 的 put,get 速度
         TestIntHash.testIntHash( initialCapacity );
         System.gc();
         System.gc();
@@ -20,37 +21,37 @@ public class TestIntHash {
         }
         long t2 = System.currentTimeMillis();
         for (int i = 0; i < aInitialCapacity; ++i) {
-            if( hash.get( i ) != Boolean.TRUE ){
+            if (hash.get( i ) != Boolean.TRUE) {
                 throw new Error();
             }
         }
         for (int i = aInitialCapacity; i < 1_0000_0000; ++i) {
-            if( hash.get( i ) != null ){
+            if (hash.get( i ) != null) {
                 throw new Error();
             }
         }
         long t3 = System.currentTimeMillis();
-        System.err.println( "<IntMap>: put spend:" + (t2 - t1) + " ms, get spend:" + (t3 - t2) + " ms" );
+        System.err.println( "<IntMap>: put spend:[" + (t2 - t1) + "]ms, 平均[" + aInitialCapacity * 1000.0 / (t2 - t1) + "]put次/秒, get spend:[" + (t3 - t2) + "]ms, 平均[" + 1_0000_0000 * 1000.0 / (t3 - t2) + "]get次/秒" );
     }
 
     private static void testHashMap(int aInitialCapacity) {
         long t1 = System.currentTimeMillis();
-        HashMap<Integer, Boolean> hash = new HashMap<Integer, Boolean>();
+        HashMap<Integer, Boolean> hash = new HashMap<Integer, Boolean>( aInitialCapacity );
         for (int i = 0; i < aInitialCapacity; ++i) {
             hash.put( i, Boolean.TRUE );
         }
         long t2 = System.currentTimeMillis();
         for (int i = 0; i < aInitialCapacity; ++i) {
-            if( hash.get( i ) != Boolean.TRUE ){
+            if (hash.get( i ) != Boolean.TRUE) {
                 throw new Error();
             }
         }
         for (int i = aInitialCapacity; i < 1_0000_0000; ++i) {
-            if( hash.get( i ) != null ){
+            if (hash.get( i ) != null) {
                 throw new Error();
             }
         }
         long t3 = System.currentTimeMillis();
-        System.err.println( "<HashMap>: put spend:" + (t2 - t1) + " ms, get spend:" + (t3 - t2) + " ms" );
+        System.err.println( "<HashMap>: put spend:[" + (t2 - t1) + "]ms, 平均[" + aInitialCapacity * 1000.0 / (t2 - t1) + "]put次/秒, get spend:[" + (t3 - t2) + "]ms, 平均[" + 1_0000_0000 * 1000.0 / (t3 - t2) + "]get次/秒" );
     }
 }
